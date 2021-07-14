4c7e5854cc6b6510005efd2846ff2a36bba7d272
Andreas Frenk <andreasfrenk@Andreass-MacBook-Pro.local>
2021-07-14 20:19:40 +0200

    initial commit

---
+++ src/components/landingpage.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import "../styles/landingpage.scss";
+
+
+class landingPage extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+  componentDidMount() {
+  }
+  render() {
+    return (
+        <div>
+            <div className="navbar flex w-full justify-between px-12 absolute">
+              
+                <div>Logo</div>
+                <div className="navbar__right">
+                <a className="navbar__link">Jetzt registrieren</a>
+            <a className="navbar__link pl-5">Anmelden</a>
+            </div>
+            </div>
+       <div className="big-banner h-screen w-full bg-cover" style={{backgroundImage: `url(https://wallpapercave.com/wp/EtxonAb.jpg)`,}}>
+
+        </div>
+        </div>
+    );
+  }
+}
+
+export default landingPage;
